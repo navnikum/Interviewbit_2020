c9486f8824440a0ec298ea18c77dcca31121e1fe
Navnit Kumar <navnitkumar1224@gmail.com>
2020-09-12 16:11:18 +0530

    Update MergeOverlappingIntervals.cpp

--- MergeOverlappingIntervals.cpp
+++ MergeOverlappingIntervals.cpp
@@ -35,3 +35,39 @@ vector<Interval> Solution::merge(vector<Interval> &A) {
     A.resize(idx+1);
     return A;
 }
+
+
+// Merge Intervals
+Asked in:  
+Google
+
+bool mycomp(Interval a, Interval b){
+    return a.start < b.start;
+}
+vector<Interval> Solution::insert(vector<Interval> &intervals, Interval newInterval) {
+    intervals.push_back(newInterval);
+    int n = intervals.size();
+    sort(intervals.begin(), intervals.end(), mycomp);
+    int idx = 0; // last in modified array;
+    for(int i = 1; i < n; i++){
+        if(intervals[idx].end >= intervals[i].start){
+            intervals[idx].end = max(intervals[idx].end, intervals[i].end);
+            intervals[idx].start = min(intervals[idx].start, intervals[i].start);
+        }
+        else{
+            idx++;
+            intervals[idx] = intervals[i];
+        }
+    }
+    intervals.resize(idx+1);
+    return intervals;
+}
+
+// OR
+
+vector<Interval> Solution::insert(vector<Interval> &intervals, Interval newInterval) {
+    intervals.push_back(newInterval);
+    return merge(intervals);
+}
+
+
