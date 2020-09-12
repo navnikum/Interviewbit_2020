3cffff9fc1a2ea1d0a6a4447ba2fc14d6d486632
Navnit Kumar <navnitkumar1224@gmail.com>
2020-09-12 15:58:25 +0530

    Update MergeOverlappingIntervals.cpp

--- MergeOverlappingIntervals.cpp
+++ MergeOverlappingIntervals.cpp
@@ -12,26 +12,26 @@ Amazon
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
+bool mycomp(Interval a, Interval b)  {
+    return a.start < b.start; 
+} 
 vector<Interval> Solution::merge(vector<Interval> &A) {
     int n = A.size();
-    // in decreasing order of start time
-    sort(A.begin(), A.end(), [](const Interval &a, const Interval &b) {return a.start > b.start;});
-    int idx = 0; // store idx of last element in output array
-    for(int i = 0; i < n; i++){
-        // if not first interval and overlaps with previous
-        if(idx !=0 && A[idx-1].start <= A[i].end){
-            while(idx !=0 && A[idx-1].start <= A[i].end){
-                // merge prev and current intervals
-                A[idx-1].end = max(A[idx-1].end, A[i].end);
-                A[idx-1].start = min(A[idx-1].start, A[i].start);
-                idx--;
-            } 
+    sort(A.begin(), A.end(), mycomp);
+    int idx = 0;  // Stores index of last element in modified array
+    for(int i = 1; i < n; i++){
+        // not first interval and overlaps with the previous interval
+        if(A[idx].end >= A[i].start){
+            // merge prev and current intervals
+            A[idx].end = max(A[idx].end, A[i].end);
+            A[idx].start = min(A[idx].start, A[i].start);
         }
         else    // no overlap add to solution
+        {   idx++;
             A[idx] = A[i];
-        idx++;
+        }
+        
     }
-    A.resize(idx);
-    sort(A.begin(), A.end(), [](const Interval &a, const Interval &b) {return a.start < b.start;});
+    A.resize(idx+1);
     return A;
 }
