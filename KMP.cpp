9041bbc2b0e05988997618079482576d6cf5b433
Navnit Kumar <navnitkumar1224@gmail.com>
2020-09-13 17:58:05 +0530

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,8 +5,9 @@ void prefix_function(string s, int* pi) {
     pi[0] = 0;
     int j = 0, n = s.size();
     for (int i = 1; i < n; i++) {
+        j = pi[i-1];
         while (j && s[i] != s[j])
-            j = pi[i-1];
+            j = pi[j-1];
         if (s[i] == s[j])
             j++;
         pi[i] = j;
